35669d0866b8960571ceb2295c2f8bbc82ffef83
Peneva <dpeneva9@gmail.com>
2021-03-24 18:27:17 +0200

    Create Dashboard component

---
+++ components/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+
+const Dashboard = () => {
+    return (
+        <View>
+            <Text>Hello there :)</Text>
+            <Button title='Add player'/>
+        </View>
+    )
+}
+
+export default Dashboard;
